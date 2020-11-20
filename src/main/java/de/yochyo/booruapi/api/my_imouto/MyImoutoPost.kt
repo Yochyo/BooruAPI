f2b87ae945acae948dd9b0c8caa491cfd14a0ab9
Yochyo <herodragon25@gmail.com>
2020-11-20 16:49:02 +0100

    added MyImouto support again

--- src/main/java/de/yochyo/booruapi/api/my_imouto/MyImoutoPost.kt
+++ src/main/java/de/yochyo/booruapi/api/my_imouto/MyImoutoPost.kt
@@ -1,5 +1,65 @@
 package de.yochyo.booruapi.api.my_imouto
 
-import de.yochyo.booruapi.api.moebooru.MoebooruPost
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import de.yochyo.booruapi.api.Post
+import de.yochyo.booruapi.api.Tag
+import de.yochyo.booruapi.deserializers.LongDateDeserializer
+import de.yochyo.booruapi.utils.extension
+import kotlinx.coroutines.runBlocking
+import java.util.*
+
 //TODO comments
-typealias MyImoutoPost = MoebooruPost
+data class MyImoutoPost(
+        override val id: Int,
+        @JsonProperty("tags") override val tagString: String,
+        @JsonDeserialize(using = LongDateDeserializer::class) val createdAt: Date,
+        val creatorId: Int,
+        val author: String,
+        val change: Int,
+        val source: String,
+        val score: Int,
+        val md5: String,
+        override val fileSize: Int,
+        val fileUrl: String,
+        val isShownInIndex: Boolean,
+        val previewUrl: String,
+        val previewWidth: Int,
+        val previewHeight: Int,
+        val actualPreviewWidth: Int,
+        val actualPreviewHeight: Int,
+        val sampleUrl: String,
+        val sampleWidth: Int,
+        val sampleHeight: Int,
+        val sampleFileSize: Int,
+        val jpegUrl: String,
+        val jpegWidth: Int,
+        val jpegHeight: Int,
+        val jpegFileSize: Int,
+        override val rating: String,
+        val hasChildren: Boolean,
+        val parentId: Int?,
+        val status: String,
+        override val width: Int,
+        override val height: Int,
+        val isHeld: Boolean,
+        //TODO val framesPendingString: String,
+        //TODO val framesPending: String,
+        //TODO val framesString: String,
+        //TODO val frames: String,
+        var myImoutoApi: MyImoutoApi? = null,
+) : Post(id, fileUrl.extension(), width, height, rating, fileSize, fileUrl, sampleUrl, previewUrl, tagString) {
+
+    private val _tags: List<Tag> by lazy {
+        val finalApi = myImoutoApi
+        if (finalApi == null) super.getTags()
+        else runBlocking { MyImoutoUtils.parseTagsfromURL(finalApi.host, id) }
+    }
+
+    /**
+     * @return this method will return default values if no api was passed in this classes contructor.
+     */
+    @JsonIgnore
+    override fun getTags(): List<Tag> = _tags
+}

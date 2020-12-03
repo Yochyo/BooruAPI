4e40776eeb8d2b2f21bf6f05c51ffed4fd56a0bb
Yochyo <herodragon25@gmail.com>
2020-12-03 11:44:37 +0100

    fixed bug where yande.re posts's tags all had the name ""

--- src/main/java/de/yochyo/booruapi/api/moebooru/MoebooruUtils.kt
+++ src/main/java/de/yochyo/booruapi/api/moebooru/MoebooruUtils.kt
@@ -1,6 +1,7 @@
 package de.yochyo.booruapi.api.moebooru
 
 import de.yochyo.booruapi.BooruTagSelector
+import de.yochyo.booruapi.api.gelbooru_beta.GelbooruBetaTag
 import org.jsoup.nodes.Element
 
 //TODO comments
@@ -8,20 +9,19 @@ object MoebooruUtils {
     private val selector = object : BooruTagSelector<MoebooruTag, Int>() {
 
         override fun getType(element: Element): Int {
-            val type = element.attr("data-type")
-            return when (type) {
-                "general" -> MoebooruTag.MOEBOORU_GENERAL
-                "copyright" -> MoebooruTag.MOEBOORU_COPYRIGHT
-                "artist" -> MoebooruTag.MOEBOORU_ARTIST
-                "circle" -> MoebooruTag.MOEBOORU_CIRCLE
-                "style" -> MoebooruTag.MOEBOORU_META
-                "character" -> MoebooruTag.MOEBOORU_CHARACTER
-                else -> MoebooruTag.MOEBOORU_UNKNOWN
-            }
+            val type = element.className()
+            if (type.contains("tag-type-general")) return MoebooruTag.MOEBOORU_GENERAL
+            if (type.contains("tag-type-artist")) return MoebooruTag.MOEBOORU_ARTIST
+            if (type.contains("tag-type-copyright")) return MoebooruTag.MOEBOORU_COPYRIGHT
+            if (type.contains("tag-type-character")) return MoebooruTag.MOEBOORU_CHARACTER
+            if (type.contains("tag-type-style")) return MoebooruTag.MOEBOORU_META
+            if (type.contains("tag-type-circle")) return MoebooruTag.MOEBOORU_CIRCLE
+            return GelbooruBetaTag.GELBOORU_BETA_UNKNOWN
         }
 
         override fun getName(element: Element): String {
-            return element.attr("data-name")
+            val elements = element.select("a").filter { it.text() != "?" }
+            return elements.firstOrNull()?.text() ?: "null"
         }
 
         override fun toTag(name: String, type: Int): MoebooruTag {
